e1d60e73402ea3bf9801a4db67762afa19ab6769
tyw12138 <tyw12138@users.noreply.github.com>
2026-07-06 18:44:52 +0800

    简化构建脚本，移除开发依赖插件

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,13 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths";
 import { traeBadgePlugin } from 'vite-plugin-trae-solo-badge';
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/personal-website.github.io/',
   build: {
     sourcemap: 'hidden',
   },
   plugins: [
-    react({
-      babel: {
-        plugins: [
-          'react-dev-locator',
-        ],
-      },
-    }),
+    react(),
     traeBadgePlugin({
       variant: 'dark',
       position: 'bottom-right',
